d5727fed35794f00fe6840fa09cdfe8e2dfd0fed
anurse <andrew@andrewnurse.net>
2013-04-29 12:20:14 -0700

    Added quick sample of FluentAutomation

---
+++ FunctionalTests/NuGetGallery.FunctionalTests/Sample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAutomation;
+using Xunit;
+
+namespace NuGetGallery.FunctionalTests
+{
+    public class Sample : FluentTest
+    {
+        public Sample()
+        {
+            FluentAutomation.SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            I.Open("http://nuget.org");
+            I.WaitUntil(() => I.Expect.Exists("p#releaseTag"));
+            I.Expect.Text(str => str.Trim().StartsWith("This is the NuGet Gallery.")).In("p#releaseTag");
+        }
+    }
+}
